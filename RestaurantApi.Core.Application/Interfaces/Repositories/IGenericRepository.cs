ee2e587f82e8d1a912ac74372ca2a9c018dc429c
carloselena <carloselenadev@gmail.com>
2026-01-04 21:44:38 -0400

    Created GenericRepository and its interface

---
+++ RestaurantApi.Core.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantApi.Core.Domain.Common;
+
+namespace RestaurantApi.Core.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<Entity> where Entity : BaseEntity
+    {
+        Task<Entity> AddAsync(Entity entity);
+        Task<Entity> UpdateAsync(Entity entity);
+        Task DeleteAsync(Entity entity);
+        Task<ICollection<Entity>> GetAllAsync(Func<IQueryable<Entity>, IQueryable<Entity>>? includes = null);
+        Task<Entity> GetByIdAsync(int id, Func<IQueryable<Entity>, IQueryable<Entity>>? includes = null);        
+    }
+}
