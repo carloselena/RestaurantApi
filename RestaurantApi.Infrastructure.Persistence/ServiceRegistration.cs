6bb88326d429a1b3c251403a2f5d8d88daf978f1
carloselena <carloselenadev@gmail.com>
2026-01-04 22:07:58 -0400

    Dependencies for repositories injected

--- RestaurantApi.Infrastructure.Persistence/ServiceRegistration.cs
+++ RestaurantApi.Infrastructure.Persistence/ServiceRegistration.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RestaurantApi.Core.Application.Interfaces.Repositories;
 using RestaurantApi.Infrastructure.Persistence.Contexts;
+using RestaurantApi.Infrastructure.Persistence.Repositories;
 
 namespace RestaurantApi.Infrastructure.Persistence
 {
@@ -13,6 +15,14 @@ namespace RestaurantApi.Infrastructure.Persistence
             services.AddDbContext<ApplicationContext>(opt =>
                                                         opt.UseSqlServer(connectionString,
                                                         m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+
+            #region Repositories
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<IIngredientRepository, IngredientRepository>();
+            services.AddTransient<IDishRepository, DishRepository>();
+            services.AddTransient<ITableRepository, TableRepository>();
+            services.AddTransient<IOrderRepository, OrderRepository>();
+            #endregion
         }
     }
 }
