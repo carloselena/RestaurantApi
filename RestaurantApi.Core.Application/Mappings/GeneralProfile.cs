f48f8d2e9345a9144f348f65a1becb29622bffee
carloselena <carloselenadev@gmail.com>
2026-01-05 17:25:55 -0400

    Mapping between Ingredient and DTOs created

---
+++ RestaurantApi.Core.Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RestaurantApi.Core.Application.DTOs.Ingredient;
+using RestaurantApi.Core.Domain.Entities;
+
+namespace RestaurantApi.Core.Application.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            #region Ingredient
+            CreateMap<Ingredient, IngredientDTO>();
+
+            CreateMap<Ingredient, SaveIngredientDTO>()
+                .ReverseMap();
+            #endregion
+        }
+    }
+}
