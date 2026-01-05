26a05974eb3c5f7e575877afe895b3965a829743
carloselena <carloselenadev@gmail.com>
2026-01-04 21:48:04 -0400

    Created IngredientRepository inheriting GenericRepository

---
+++ RestaurantApi.Core.Application/Interfaces/Repositories/IIngredientRepository.cs
@@ -0,0 +1,8 @@
+﻿using RestaurantApi.Core.Domain.Entities;
+
+namespace RestaurantApi.Core.Application.Interfaces.Repositories
+{
+    public interface IIngredientRepository : IGenericRepository<Ingredient>
+    {
+    }
+}
