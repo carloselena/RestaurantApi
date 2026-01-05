5f8f49b553cdc58fa06ad0b3f5ee396e58a56b76
carloselena <carloselenadev@gmail.com>
2026-01-05 13:49:16 -0400

    OrderService created inheriting GenericService and implementing its own methods, and its DTOs

---
+++ RestaurantApi.Core.Application/DTOs/Order/ChangeOrderStatusDTO.cs
@@ -0,0 +1,9 @@
+﻿using RestaurantApi.Core.Application.Enums;
+
+namespace RestaurantApi.Core.Application.DTOs.Order
+{
+    public class ChangeOrderStatusDTO
+    {
+        public OrderStatus Status = OrderStatus.COMPLETADA;
+    }
+}
---
+++ RestaurantApi.Core.Application/DTOs/Order/UpdateOrderDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace RestaurantApi.Core.Application.DTOs.Order
+{
+    public class UpdateOrderDTO
+    {
+        public List<int> DishesIds { get; set; }
+    }
+}
