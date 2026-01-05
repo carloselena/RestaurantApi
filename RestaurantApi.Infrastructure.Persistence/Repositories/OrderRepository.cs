01859e05b25af8d8b9ceb50affe4a30888dfef8b
carloselena <carloselenadev@gmail.com>
2026-01-04 21:54:37 -0400

    Created OrderRepository inheriting GenericRepository

---
+++ RestaurantApi.Infrastructure.Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantApi.Core.Application.Interfaces.Repositories;
+using RestaurantApi.Core.Domain.Entities;
+using RestaurantApi.Infrastructure.Persistence.Contexts;
+
+namespace RestaurantApi.Infrastructure.Persistence.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public OrderRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
