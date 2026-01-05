3e9625214a886453da30364bdea8c2b67c400565
carloselena <carloselenadev@gmail.com>
2026-01-04 21:50:21 -0400

    Created DishRepository inheriting GenericRepository

---
+++ RestaurantApi.Infrastructure.Persistence/Repositories/DishRepository.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantApi.Core.Application.Interfaces.Repositories;
+using RestaurantApi.Core.Domain.Entities;
+using RestaurantApi.Infrastructure.Persistence.Contexts;
+
+namespace RestaurantApi.Infrastructure.Persistence.Repositories
+{
+    public class DishRepository : GenericRepository<Dish>, IDishRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public DishRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
