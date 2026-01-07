1c8bbf71998b0dd680787b8c6f69ef9ae3a3fa6a
carloselena <carloselenadev@gmail.com>
2026-01-06 20:12:36 -0400

    EmailService created

---
+++ RestaurantApi.Core.Application/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using RestaurantApi.Core.Application.DTOs.Email;
+
+namespace RestaurantApi.Core.Application.Interfaces.Services
+{
+    public interface IEmailService
+    {
+        Task SendAsync(EmailRequest emailRequest);
+    }
+}
---
+++ RestaurantApi.Infrastructure.Shared/Services/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using RestaurantApi.Core.Application.DTOs.Email;
+using RestaurantApi.Core.Application.Interfaces.Services;
+using RestaurantApi.Core.Domain.Settings;
+
+namespace RestaurantApi.Infrastructure.Shared.Services
+{
+    public class EmailService : IEmailService
+    {
+        private MailSettings _mailSettings { get; }
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendAsync(EmailRequest emailRequest)
+        {
+            try
+            {
+                MimeMessage email = new();
+                email.From.Add(new MailboxAddress(_mailSettings.FromName, _mailSettings.From));
+                email.To.Add(MailboxAddress.Parse(emailRequest.To));
+                email.Subject = emailRequest.Subject;
+                BodyBuilder builder = new() { HtmlBody = emailRequest.Body };
+                email.Body = builder.ToMessageBody();
+
+                using SmtpClient smtp = new();
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.User, _mailSettings.Pass);
+                await smtp.SendAsync(email);
+                smtp.Disconnect(true);
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
