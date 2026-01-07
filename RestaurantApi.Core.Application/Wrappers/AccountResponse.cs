d1d55adba6b05e54744f1a8bed1d78599447e46b
carloselena <carloselenadev@gmail.com>
2026-01-06 20:52:33 -0400

    AccountResponse wrapper created in order to return standarized responses from AccountService

---
+++ RestaurantApi.Core.Application/Wrappers/AccountResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace RestaurantApi.Core.Application.Wrappers
+{
+    public class AccountResponse
+    {
+        public bool HasError { get; init; }
+        public string? Error { get; init; }
+
+        protected AccountResponse() { }
+
+        public static AccountResponse Success()
+            => new() { HasError = false };
+
+        public static AccountResponse Fail(string error)
+            => new() { HasError = true, Error = error };
+    }
+
+    public class AccountResponse<T> : AccountResponse
+    {
+        public T? Data { get; init; }
+
+        protected AccountResponse() { }
+
+        public static AccountResponse<T> Success(T data)
+            => new() { HasError = false, Data = data };
+
+        public new static AccountResponse<T> Fail(string error)
+            => new() { HasError = true, Error = error };
+    }
+}
