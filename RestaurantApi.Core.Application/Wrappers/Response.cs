b2137e153252e742d40db9afd77186df1a59d07e
carloselena <carloselenadev@gmail.com>
2026-01-08 10:30:32 -0400

    Wrapper Response<T> created for returning standardized responses

---
+++ RestaurantApi.Core.Application/Wrappers/Response.cs
@@ -0,0 +1,34 @@
+﻿namespace RestaurantApi.Core.Application.Wrappers
+{
+    public class Response<T>
+    {
+        public bool Succeeded { get; set; }
+        public string? Message { get; set; }
+        public List<string>? Errors { get; set; }
+        public T? Data { get; set; }
+
+        protected Response() { }
+
+        public static Response<T> Success(T data, string? message = null)
+            => new()
+            {
+                Succeeded = true,
+                Data = data,
+                Message = message
+            };
+
+        public static Response<T> Fail(string error)
+            => new()
+            {
+                Succeeded = false,
+                Errors = new() { error }
+            };
+
+        public static Response<T> Fail(List<string> errors)
+            => new()
+            {
+                Succeeded = false,
+                Errors = errors
+            };
+    }
+}
