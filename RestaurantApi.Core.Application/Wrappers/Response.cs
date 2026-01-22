654fa80667bb325e03b4740d6c64e819e21db5e6
carloselena <carloselenadev@gmail.com>
2026-01-22 13:59:50 -0400

    Response wrapper modified, fail status will be handled by exceptions

--- RestaurantApi.Core.Application/Wrappers/Response.cs
+++ RestaurantApi.Core.Application/Wrappers/Response.cs
@@ -2,33 +2,13 @@
 {
     public class Response<T>
     {
-        public bool Succeeded { get; set; }
         public string? Message { get; set; }
-        public List<string>? Errors { get; set; }
         public T? Data { get; set; }
 
-        protected Response() { }
-
-        public static Response<T> Success(T data, string? message = null)
-            => new()
-            {
-                Succeeded = true,
-                Data = data,
-                Message = message
-            };
-
-        public static Response<T> Fail(string error)
-            => new()
-            {
-                Succeeded = false,
-                Errors = new() { error }
-            };
-
-        public static Response<T> Fail(List<string> errors)
-            => new()
-            {
-                Succeeded = false,
-                Errors = errors
-            };
+        public Response(T data, string? message = null)
+        {
+            Data = data;
+            Message = message;
+        }
     }
 }
